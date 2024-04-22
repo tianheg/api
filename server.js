57fd27aaf90c5e45578357659e4cd8c6da48e4b8
tianheg <i@tianheg.co>
2024-04-22 22:25:10 +0800

    fix: disable cors

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.register(
     if (err) throw err;
   }
 );
-app.register(import('@fastify/cors'));
+// app.register(import('@fastify/cors'));
 app.register(import('@fastify/helmet'));
 app.register(import('@fastify/rate-limit'), {
   max: 100,
