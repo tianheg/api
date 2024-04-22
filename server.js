1ee851b4296cb05d5fad67444a1d49d31fa01232
tianheg <i@tianheg.co>
2024-04-22 21:28:31 +0800

    feat: add plugins

--- server.js
+++ server.js
@@ -5,13 +5,24 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 // Require the framework
+import http from 'node:http';
 import Fastify from 'fastify';
+import fastifyCaching from '@fastify/caching';
 
 // Instantiate Fastify with some config
 const app = Fastify({
   logger: true,
 });
 
+// plugins
+app.register(import('@fastify/cors'));
+app.register(
+  import('@fastify/caching'),
+  { privacy: fastifyCaching.privacy.PUBLIC },
+  (err) => {
+    if (err) throw err;
+  }
+);
 // Register your application as a normal plugin.
 app.register(import('./src/app.js'));
 
@@ -19,6 +30,10 @@ if (process.env.NODE_ENV === 'development') {
   const start = async () => {
     try {
       await app.listen({ port: 3000 });
+
+      http.get('http://127.0.0.1:3000/', (res) => {
+        console.log(res.headers['cache-control']);
+      });
     } catch (err) {
       app.log.error(err);
       process.exit(1);
