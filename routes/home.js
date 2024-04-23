7d94d2204602cebfea70086066cde3e3d9af9511
tianheg <i@tianheg.co>
2024-04-23 21:35:11 +0800

    feat: add new route /books

--- routes/home.js
+++ routes/home.js
@@ -1,9 +1,20 @@
 export default function (f, opts, next) {
   f.get('/', (request, reply) => {
+    const protocol = process.env.NODE_ENV === 'development' ? 'http' : 'https';
+    const baseUrl = `${protocol}://${request.headers.host}`;
+    const routes = [
+      `${baseUrl}/words`,
+      `${baseUrl}/books`,
+      `${baseUrl}/movies`,
+      `${baseUrl}/series`,
+      `${baseUrl}/music`,
+      `${baseUrl}/feeds`,
+    ];
+
     reply.send({
-      intro:
-        "tianheg's api homepage, include words, books, movies, series, music",
+      intro: "tianheg's api homepage",
       repo: 'https://github.com/tianheg/api/, powered by Fastify(https://fastify.dev/), deployed on Vercel(https://vercel.com/)',
+      routes: routes,
     });
   });
 
