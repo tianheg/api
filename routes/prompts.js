3f104ed0ae8d68d592d3c1d0f2810ced8d0e3a67
tianheg <i@tianheg.co>
2024-04-27 20:13:11 +0800

    update prompts

--- routes/prompts.js
+++ routes/prompts.js
@@ -8,8 +8,13 @@ export default function (f, opts, next) {
   next();
 }
 
-// TODO
-// https://prompts.chat/
-// 心理学 prompt https://twitter.com/daydayuuup/status/1746012683625746852
-// Refer
-// https://baoyu.io/blog/prompt-engineering/how-to-write-high-quality-prompt
+/**
+ * TODO
+ * https://prompts.chat/
+ * 心理学 prompt https://twitter.com/daydayuuup/status/1746012683625746852
+ *
+ * Refer
+ * https://baoyu.io/blog/prompt-engineering/how-to-write-high-quality-prompt
+ * https://www.wangdu.site/software/ai/2077.html
+ * https://m.okjike.com/originalPosts/65937c9212ed2fda6838d586?s=eyJ1IjoiNTZkMDFjNGE4ZTNmZGMxMTAwMWQ4NWEzIiwiZCI6NX0%3D&amp%3Butm_source=wechat_session
+ */
