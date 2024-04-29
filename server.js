9c1b93a3503895e4591379584c2cd25cefe78eca
tianheg <i@tianheg.co>
2024-04-29 20:22:46 +0800

    add biome lint tool

--- server.js
+++ server.js
@@ -1,65 +1,63 @@
-'use strict';
-
-import Fastify from 'fastify';
+import Fastify from "fastify";
 
 // logger
-import pino from 'pino';
-import pretty from 'pino-pretty';
+import pino from "pino";
+import pretty from "pino-pretty";
 const stream = pretty({
-  translateTime: 'SYS:HH:MM:ss Z',
-  messageFormat: '{msg} {req.method} {req.url}',
-  include: 'time,pid,level',
-  hideObject: true,
-  colorize: true,
+	translateTime: "SYS:HH:MM:ss Z",
+	messageFormat: "{msg} {req.method} {req.url}",
+	include: "time,pid,level",
+	hideObject: true,
+	colorize: true,
 });
-const logger = pino({ level: 'info' }, stream);
+const logger = pino({ level: "info" }, stream);
 
 const app = Fastify({ logger });
 
 // plugins
-import { apitallyPlugin } from 'apitally/fastify';
+import { apitallyPlugin } from "apitally/fastify";
 await app.register(apitallyPlugin, {
-  clientId: '6317af0b-5121-408a-adf3-5d020cd0b36f',
-  env: 'prod', // or "dev"
+	clientId: "6317af0b-5121-408a-adf3-5d020cd0b36f",
+	env: "prod", // or "dev"
 });
-app.register(import('@fastify/swagger'));
-app.register(import('@fastify/compress'), { encodings: ['gzip'] });
-app.register(import('@fastify/cors'));
-app.register(import('@fastify/helmet'));
-app.register(import('@fastify/rate-limit'), {
-  max: 100,
-  timeWindow: '1 minute',
+app.register(import("@fastify/swagger"));
+app.register(import("@fastify/compress"), { encodings: ["gzip"] });
+app.register(import("@fastify/cors"));
+app.register(import("@fastify/helmet"));
+app.register(import("@fastify/rate-limit"), {
+	max: 100,
+	timeWindow: "1 minute",
 });
-app.register(import('@fastify/swagger-ui'), {
-  routePrefix: '/doc',
-  uiConfig: {
-    docExpansion: 'full',
-    deepLinking: false,
-  },
-  uiHooks: {
-    onRequest: function (request, reply, next) {
-      next();
-    },
-    preHandler: function (request, reply, next) {
-      next();
-    },
-  },
-  staticCSP: true,
-  transformStaticCSP: (header) => header,
-  transformSpecification: (swaggerObject, request, reply) => {
-    return swaggerObject;
-  },
-  transformSpecificationClone: true,
+app.register(import("@fastify/swagger-ui"), {
+	routePrefix: "/doc",
+	uiConfig: {
+		docExpansion: "full",
+		deepLinking: false,
+	},
+	uiHooks: {
+		onRequest: (request, reply, next) => {
+			next();
+		},
+		preHandler: (request, reply, next) => {
+			next();
+		},
+	},
+	staticCSP: true,
+	transformStaticCSP: (header) => header,
+	transformSpecification: (swaggerObject, request, reply) => {
+		return swaggerObject;
+	},
+	transformSpecificationClone: true,
 });
 
 // routes
-import booksData from './data/books.js';
-import feedsData from './data/feeds.js';
-import moviesData from './data/movies.js';
-import musicData from './data/music.js';
-import promptsData from './data/prompts.js';
-import seriesData from './data/series.js';
-import wordsData from './data/words.js';
+import booksData from "./data/books.js";
+import feedsData from "./data/feeds.js";
+import moviesData from "./data/movies.js";
+import musicData from "./data/music.js";
+import promptsData from "./data/prompts.js";
+import seriesData from "./data/series.js";
+import wordsData from "./data/words.js";
 
 /**
  * Function that paginates data based on the given page and limit.
@@ -70,37 +68,37 @@ import wordsData from './data/words.js';
  * @return {Object} An object containing paginated data, current page, limit, total data count, total pages.
  */
 function getPaginatedData(data, searchTerm, page, limit) {
-  // Filter data if searchTerm is provided
-  const filteredData = searchTerm
-    ? data.filter((item) =>
-        JSON.stringify(item).toLowerCase().includes(searchTerm.toLowerCase())
-      )
-    : data;
+	// Filter data if searchTerm is provided
+	const filteredData = searchTerm
+		? data.filter((item) =>
+				JSON.stringify(item).toLowerCase().includes(searchTerm.toLowerCase()),
+			)
+		: data;
 
-  // Calculate pagination as before
-  const startIndex = (page - 1) * limit;
-  const endIndex = Math.min(startIndex + limit, filteredData.length);
-  const paginatedData = filteredData.slice(startIndex, endIndex);
-  const totalPages = Math.ceil(filteredData.length / limit);
+	// Calculate pagination as before
+	const startIndex = (page - 1) * limit;
+	const endIndex = Math.min(startIndex + limit, filteredData.length);
+	const paginatedData = filteredData.slice(startIndex, endIndex);
+	const totalPages = Math.ceil(filteredData.length / limit);
 
-  return {
-    page,
-    limit,
-    total: filteredData.length,
-    totalPages,
-    data: paginatedData,
-  };
+	return {
+		page,
+		limit,
+		total: filteredData.length,
+		totalPages,
+		data: paginatedData,
+	};
 }
 
 const paginationSchema = {
-  querystring: {
-    type: 'object',
-    properties: {
-      page: { type: 'integer', minimum: 1, default: 1 },
-      limit: { type: 'integer', minimum: 1, maximum: 100, default: 10 },
-      search: { type: 'string', default: '' },
-    },
-  },
+	querystring: {
+		type: "object",
+		properties: {
+			page: { type: "integer", minimum: 1, default: 1 },
+			limit: { type: "integer", minimum: 1, maximum: 100, default: 10 },
+			search: { type: "string", default: "" },
+		},
+	},
 };
 
 /**
@@ -112,56 +110,56 @@ const paginationSchema = {
  * @return {Promise} A promise containing the paginated data.
  */
 function createRoute(path, data, opts) {
-  app.get(path, { schema: opts.schema }, async (request, reply) => {
-    const { page, limit, search } = request.query;
-    return getPaginatedData(data, search, page, limit);
-  });
+	app.get(path, { schema: opts.schema }, async (request, reply) => {
+		const { page, limit, search } = request.query;
+		return getPaginatedData(data, search, page, limit);
+	});
 }
 
-app.get('/', (request, reply) => {
-  const protocol = process.env.NODE_ENV === 'development' ? 'http' : 'https';
-  const baseUrl = `${protocol}://${request.headers.host}`;
-  const routes = [
-    `${baseUrl}/words`,
-    `${baseUrl}/books`,
-    `${baseUrl}/movies`,
-    `${baseUrl}/series`,
-    `${baseUrl}/music`,
-    `${baseUrl}/feeds`,
-    `${baseUrl}/prompts`,
-  ];
+app.get("/", (request, reply) => {
+	const protocol = process.env.NODE_ENV === "development" ? "http" : "https";
+	const baseUrl = `${protocol}://${request.headers.host}`;
+	const routes = [
+		`${baseUrl}/words`,
+		`${baseUrl}/books`,
+		`${baseUrl}/movies`,
+		`${baseUrl}/series`,
+		`${baseUrl}/music`,
+		`${baseUrl}/feeds`,
+		`${baseUrl}/prompts`,
+	];
 
-  reply.send({
-    repo: 'https://github.com/tianheg/api/',
-    tech: 'https://fastify.dev/',
-    deploy: 'https://vercel.com/',
-    routes: routes,
-  });
+	reply.send({
+		repo: "https://github.com/tianheg/api/",
+		tech: "https://fastify.dev/",
+		deploy: "https://vercel.com/",
+		routes: routes,
+	});
 });
 
-createRoute('/books', booksData, { schema: paginationSchema });
-createRoute('/feeds', feedsData, { schema: paginationSchema });
-createRoute('/movies', moviesData, { schema: paginationSchema });
-createRoute('/music', musicData, { schema: paginationSchema });
-createRoute('/prompts', promptsData, { schema: paginationSchema });
-createRoute('/series', seriesData, { schema: paginationSchema });
-createRoute('/words', wordsData, { schema: paginationSchema });
+createRoute("/books", booksData, { schema: paginationSchema });
+createRoute("/feeds", feedsData, { schema: paginationSchema });
+createRoute("/movies", moviesData, { schema: paginationSchema });
+createRoute("/music", musicData, { schema: paginationSchema });
+createRoute("/prompts", promptsData, { schema: paginationSchema });
+createRoute("/series", seriesData, { schema: paginationSchema });
+createRoute("/words", wordsData, { schema: paginationSchema });
 
-if (process.env.NODE_ENV === 'development') {
-  /**
-   * A function that asynchronously starts the application listening on port 3000.
-   *
-   * @return {Promise} A promise that resolves when the application starts listening successfully.
-   */
-  const start = async () => {
-    try {
-      await app.listen({ port: 3000 });
-    } catch (err) {
-      app.log.error(err);
-      process.exit(1);
-    }
-  };
-  start();
+if (process.env.NODE_ENV === "development") {
+	/**
+	 * A function that asynchronously starts the application listening on port 3000.
+	 *
+	 * @return {Promise} A promise that resolves when the application starts listening successfully.
+	 */
+	const start = async () => {
+		try {
+			await app.listen({ port: 3000 });
+		} catch (err) {
+			app.log.error(err);
+			process.exit(1);
+		}
+	};
+	start();
 }
 
 export default app;
