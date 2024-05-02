b30193f2b0b912e2f9d44632100d23da3e587aa6
tianheg <i@tianheg.co>
2024-05-02 12:57:43 +0800

    format files with biome

--- scripts/fetchMusic.js
+++ scripts/fetchMusic.js
@@ -1,38 +1,38 @@
-import * as cheerio from "cheerio";
 import * as fs from "node:fs";
+import * as cheerio from "cheerio";
 
 const url = "https://tianheg.co/tags/音乐/";
 
 fetch(url)
-	.then((response) => response.text())
-	.then((html) => {
-		const $ = cheerio.load(html);
-		const links = $(".list-item-title");
+  .then((response) => response.text())
+  .then((html) => {
+    const $ = cheerio.load(html);
+    const links = $(".list-item-title");
 
-		const jsonArr = [];
+    const jsonArr = [];
 
-		links.each((index, element) => {
-			const linkText = $(element).text();
-			const linkHref = $(element).attr("href");
+    links.each((index, element) => {
+      const linkText = $(element).text();
+      const linkHref = $(element).attr("href");
 
-			const linkData = {
-				text: linkText,
-				href: `https://tianheg.co${linkHref}`,
-			};
+      const linkData = {
+        text: linkText,
+        href: `https://tianheg.co${linkHref}`,
+      };
 
-			jsonArr.push(linkData);
-		});
+      jsonArr.push(linkData);
+    });
 
-		const jsonData = JSON.stringify(jsonArr, null, 2);
+    const jsonData = JSON.stringify(jsonArr, null, 2);
 
-		fs.writeFile("./data/music.js", `export default ${jsonData}`, (err) => {
-			if (err) {
-				console.error("Error writing JSON file:", err);
-			} else {
-				console.log("JSON file has been written successfully.");
-			}
-		});
-	})
-	.catch((error) => {
-		console.error("Error fetching the VitePress site:", error);
-	});
+    fs.writeFile("./data/music.js", `export default ${jsonData}`, (err) => {
+      if (err) {
+        console.error("Error writing JSON file:", err);
+      } else {
+        console.log("JSON file has been written successfully.");
+      }
+    });
+  })
+  .catch((error) => {
+    console.error("Error fetching the VitePress site:", error);
+  });
