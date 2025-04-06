fb0f0645ece02e1f934447e946c8b1b31239fd57
tianheg <i@tianheg.co>
2025-04-06 23:53:06 +0800

    recover CORS rules

--- plugins/cors.js
+++ plugins/cors.js
@@ -2,5 +2,19 @@ import cors from "@fastify/cors";
 import fp from "fastify-plugin";
 
 export default fp(async (app, _) => {
-  await app.register(cors);
+  await app.register(cors, {
+    origin: (origin, cb) => {
+      const allowedOrigins = [
+        "https://lifebook.tianheg.org",
+        "http://localhost:5173",
+      ];
+      if (!origin || allowedOrigins.includes(origin)) {
+        cb(null, true);
+      } else {
+        cb(new Error("CORS not allowed"), false);
+      }
+    },
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type"],
+  });
 });
