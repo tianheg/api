1f40c1df09f04123d2bb0a0da1275e23c9a6f34f
tianheg <i@tianheg.co>
2025-04-06 23:10:24 +0800

    fix CORS

--- plugins/cors.js
+++ plugins/cors.js
@@ -2,13 +2,5 @@ import fp from "fastify-plugin";
 import cors from "@fastify/cors";
 
 export default fp(async (app, _) => {
-  // Apply CORS globally to all routes
-  await app.register(cors, {
-    origin: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Include OPTIONS
-    allowedHeaders: ['Content-Type'], // Explicitly allow headersa
-  });
-  
-  // Log when CORS is initialized
-  app.log.info('CORS plugin registered');
+  await app.register(cors);
 });
