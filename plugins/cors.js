4a526d271c1465c9b56274f215885df669fb3499
tianheg <i@tianheg.co>
2025-04-06 23:07:08 +0800

    fix CORS

--- plugins/cors.js
+++ plugins/cors.js
@@ -4,19 +4,9 @@ import cors from "@fastify/cors";
 export default fp(async (app, _) => {
   // Apply CORS globally to all routes
   await app.register(cors, {
-    origin: (origin, cb) => {
-      const allowedOrigins = [
-        'https://lifebook.tianheg.org',
-        'http://localhost:5173',
-      ];
-      if (!origin || allowedOrigins.includes(origin)) {
-        cb(null, true);
-      } else {
-        cb(new Error('CORS not allowed'), false);
-      }
-    },
+    origin: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Include OPTIONS
-    allowedHeaders: ['Content-Type'], // Explicitly allow headers
+    allowedHeaders: ['Content-Type'], // Explicitly allow headersa
   });
   
   // Log when CORS is initialized
