159e5dd9c45f9411d5289b7d5228391f521e0335
tianheg <i@tianheg.co>
2025-05-05 00:14:24 +0800

    chore: simplify the error UI

--- ui/src/views/MusicView.vue
+++ ui/src/views/MusicView.vue
@@ -147,10 +147,6 @@ onMounted(fetchItems);
   <div class="card bg-base-100">
     <div class="card-body">
       <h1 class="card-title text-2xl mb-6 text-primary">Music Management</h1>
-      <div v-if="error" class="alert alert-error mb-4 text-error-content">
-        <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
-        <span>{{ error }}</span>
-      </div>
       <div v-if="loading" class="flex justify-center my-8">
         <span class="loading loading-spinner loading-lg text-primary"></span>
       </div>
