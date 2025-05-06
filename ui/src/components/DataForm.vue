c49ba9f703d298e296ebb45540f6306223672366
tianheg <i@tianheg.co>
2025-05-07 00:51:29 +0800

    chore: clean the code

--- ui/src/components/DataForm.vue
+++ ui/src/components/DataForm.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { computed, nextTick, onMounted, ref, watch } from 'vue';
+import { nextTick, onMounted, ref, watch } from 'vue';
 
 const props = defineProps({
   fields: { type: Array, required: true },
