65ae5b041a8a974ea881f2825babb44405ae0ced
tianheg <i@tianheg.co>
2025-08-17 16:24:18 +0800

    fix hidden menu

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -17,51 +17,30 @@ import { RouterLink, RouterView } from "vue-router";
           <!-- Desktop Navigation -->
           <div class="navbar-center lg:flex">
             <nav class="flex items-center gap-4">
-              <RouterLink 
-                to="/" 
-                class="btn btn-ghost text-lg text-base-content"
-                active-class="btn-primary text-primary-content"
-              >
+              <RouterLink to="/" class="btn btn-ghost text-lg text-base-content"
+                active-class="btn-primary text-primary-content">
                 Home
               </RouterLink>
-
-              <template>
-                <RouterLink 
-                  to="/feeds" 
-                  class="btn btn-ghost text-lg text-base-content"
-                  active-class="btn-primary text-primary-content"
-                >
-                  Feeds
-                </RouterLink>
-                <RouterLink 
-                  to="/watch" 
-                  class="btn btn-ghost text-lg text-base-content"
-                  active-class="btn-primary text-primary-content"
-                >
-                  Watch
-                </RouterLink>
-                <RouterLink 
-                  to="/music" 
-                  class="btn btn-ghost text-lg text-base-content"
-                  active-class="btn-primary text-primary-content"
-                >
-                  Music
-                </RouterLink>
-                <RouterLink 
-                  to="/musicals" 
-                  class="btn btn-ghost text-lg text-base-content"
-                  active-class="btn-primary text-primary-content"
-                >
-                  Musicals
-                </RouterLink>
-                <RouterLink 
-                  to="/sentences" 
-                  class="btn btn-ghost text-lg text-base-content"
-                  active-class="btn-primary text-primary-content"
-                >
-                  Sentences
-                </RouterLink>
-              </template>
+              <RouterLink to="/feeds" class="btn btn-ghost text-lg text-base-content"
+                active-class="btn-primary text-primary-content">
+                Feeds
+              </RouterLink>
+              <RouterLink to="/watch" class="btn btn-ghost text-lg text-base-content"
+                active-class="btn-primary text-primary-content">
+                Watch
+              </RouterLink>
+              <RouterLink to="/music" class="btn btn-ghost text-lg text-base-content"
+                active-class="btn-primary text-primary-content">
+                Music
+              </RouterLink>
+              <RouterLink to="/musicals" class="btn btn-ghost text-lg text-base-content"
+                active-class="btn-primary text-primary-content">
+                Musicals
+              </RouterLink>
+              <RouterLink to="/sentences" class="btn btn-ghost text-lg text-base-content"
+                active-class="btn-primary text-primary-content">
+                Sentences
+              </RouterLink>
             </nav>
           </div>
 
@@ -69,19 +48,31 @@ import { RouterLink, RouterView } from "vue-router";
           <div class="navbar-end lg:hidden">
             <div class="dropdown dropdown-end">
               <label tabindex="0" class="btn btn-ghost text-base-content">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
+                  stroke="currentColor">
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
                 </svg>
               </label>
-              <ul tabindex="0" class="dropdown-content z-[1] menu p-2 shadow bg-base-300 rounded-box w-52 mt-4 text-base-content">
-                <li><RouterLink to="/">Home</RouterLink></li>
-                <template>
-                  <li><RouterLink to="/feeds">Feeds</RouterLink></li>
-                  <li><RouterLink to="/watch">Watch</RouterLink></li>
-                  <li><RouterLink to="/music">Music</RouterLink></li>
-                  <li><RouterLink to="/musicals">Musicals</RouterLink></li>
-                  <li><RouterLink to="/sentences">Sentences</RouterLink></li>
-                </template>
+              <ul tabindex="0"
+                class="dropdown-content z-[1] menu p-2 shadow bg-base-300 rounded-box w-52 mt-4 text-base-content">
+                <li>
+                  <RouterLink to="/">Home</RouterLink>
+                </li>
+                <li>
+                  <RouterLink to="/feeds">Feeds</RouterLink>
+                </li>
+                <li>
+                  <RouterLink to="/watch">Watch</RouterLink>
+                </li>
+                <li>
+                  <RouterLink to="/music">Music</RouterLink>
+                </li>
+                <li>
+                  <RouterLink to="/musicals">Musicals</RouterLink>
+                </li>
+                <li>
+                  <RouterLink to="/sentences">Sentences</RouterLink>
+                </li>
               </ul>
             </div>
           </div>
